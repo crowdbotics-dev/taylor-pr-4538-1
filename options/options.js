071b06727a83bb990e81254c6d8b8b5842c86b16
Crowdbotics <engineering@crowdbotics.com>
2024-01-03 00:32:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "taylor-pr-4538-1",
+  url: "https://taylor-pr-4538-1.botics.co",
+  api: "https://taylor-pr-4538-1.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
